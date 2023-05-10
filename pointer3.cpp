057e01b19bb22388602de7a3051f907103704cd1
adhamcahyo <127497339+adhamcahyo@users.noreply.github.com>
2023-05-10 11:35:35 +0700

    pointer3.cpp

--- pointer3.cpp
+++ pointer3.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+class siswa;
+
+class orang {
+private;
+	string nama;
+public;
+	void setNama(string pNama);
+	friend class siswa;
+);
+
+class siswa {
+private;
+	int id;
+public;
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a){
+	cout << id << endl << a.nama;
+}
+void siswa::setId(int pId) {
+	id = pId;
+}
+void orang::setNama(string pNama){
+	nama = pNama;
+}
+int main()
+{
+orang o;
+o.setName("Joko Kumat");
+siswa s;
+s.setId(1);
+s.displayAll(0);
+cout << endl;
+system("pause");
+return 0;
+}
